a0fe9deed2e9e5013f3fd70b8c7db3ca5fd17eb0
sandeep-bandikatla <sandeepbandikatla@gmail.com>
2025-04-10 09:41:39 +0530

    haha

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from datetime import datetime
 import pytz
 
@@ -15,12 +16,14 @@ class TimeResource(Resource):
 
 class MyFlask(Flask):
     def add_api(self):
-        from flask_restful import Api
         self.api = Api(self)
-        self.api.add_resource(TimeResource, "/api/v1/time")
+        self.api.add_resource(TimeResource, "/codegnana/time")
 
 app = MyFlask(__name__)
 app.add_api()
+
+# Proper CORS setup with support_credentials=True
+CORS(app, supports_credentials=True)
 
 if __name__ == '__main__':
     app.run()
